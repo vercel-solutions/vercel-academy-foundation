c903f71ea65e636e1bc63330a36fc8eacf430369
goncy <gonzalo.pozzo4@gmail.com>
2025-08-12 17:57:34 -0300

    Initial commit

---
+++ apps/blog/next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloudflare-ipfs.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'loremflickr.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
